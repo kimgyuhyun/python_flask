96084a4543df8d4427a9fb3ea54f1cba0ba6d3db
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-12 00:04:30 +0900

    앵커 엘리먼트로 이동할 수 있도록 redirect 수정

--- myproject/pybo/views/answer_views.py
+++ myproject/pybo/views/answer_views.py
@@ -22,7 +22,8 @@ def create(question_id):
                         create_date=datetime.now(), user=g.user)
         question.answer_set.append(answer)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=question_id))
+        return redirect('{}#answer_{}'.format(
+            url_for('question.detail', question_id=question_id), answer.id))
     return render_template('question/question_detail.html', question=question, form=form)
 
 
@@ -39,7 +40,8 @@ def modify(answer_id):
             form.populate_obj(answer)
             answer.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=answer.question.id))
+            return redirect('{}#answer_{}'.format(
+                url_for('question.detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(obj=answer)
     return render_template('answer/answer_form.html', form=form)
