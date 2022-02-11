791ac8209379bd81bd7835ac367b7877b4e7bfa6
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-11 23:39:45 +0900

    테이블 객체 answer_voter 생성 후 Answer 모델 수정

--- myproject/pybo/models.py
+++ myproject/pybo/models.py
@@ -8,6 +8,14 @@ question_voter = db.Table(
         'question.id', ondelete='CASCADE'), primary_key=True)
 )
 
+answer_voter = db.Table(
+    'answer_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey(
+        'user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey(
+        'answer.id', ondelete='CASCADE'), primary_key=True)
+)
+
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +41,8 @@ class Answer(db.Model):
         'user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('answer_set'))
     modify_date = db.Column(db.DateTime(), nullable=True)
+    voter = db.relationship('User', secondary=answer_voter,
+                            backref=db.backref('answer_voter_set'))
 
 
 class User(db.Model):
