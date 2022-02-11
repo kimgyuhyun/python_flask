28350391ac91f84dfbae91a229ace948ff42d4d8
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-11 20:06:41 +0900

    데코레이터 함수 생성

--- myproject/pybo/views/auth_views.py
+++ myproject/pybo/views/auth_views.py
@@ -5,6 +5,7 @@ from werkzeug.utils import redirect
 from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
+import functools
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -58,3 +59,12 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
