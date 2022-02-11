1fc72acd9af4a74cc3c42f111083e46a7b6e5433
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-11 23:11:07 +0900

    질문 댓글 폼 작성

--- myproject/pybo/forms.py
+++ myproject/pybo/forms.py
@@ -25,3 +25,7 @@ class UserLoginForm(FlaskForm):
     username = StringField('사용자이름', validators=[
                            DataRequired(), Length(min=3, max=25)])
     password = PasswordField('비밀번호', validators=[DataRequired()])
+
+
+class CommentForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired()])
