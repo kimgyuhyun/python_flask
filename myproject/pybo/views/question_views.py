bc63093447f493c6c7a3e4fa59b613b464ef4139
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-11 20:30:52 +0900

    질문 수정 라우트 함수 modify 추가

--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, url_for, g
+from flask import Blueprint, render_template, request, url_for, g, flash
 from werkzeug.utils import redirect
 
 from pybo.models import Question
@@ -37,4 +37,23 @@ def create():
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
+    return render_template('question/question_form.html', form=form)
+
+
+@bp.route('/modify/<int:question_id>', methods=('GET', 'POST'))
+@login_required
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('수정권한이 없습니다')
+        return redirect(url_for('question.detail', question_id=question_id))
+    if request.method == 'POST':  # POST 요청
+        form = QuestionForm()
+        if form.validate_on_submit():
+            form.populate_obj(question)
+            question.modify_date = datetime.now()  # 수정일시 저장
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=question_id))
+    else:  # GET 요청
+        form = QuestionForm(obj=question)
     return render_template('question/question_form.html', form=form)
