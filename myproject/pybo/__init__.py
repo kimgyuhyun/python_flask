ced751b09563a0ccdecf3832c521b4ce30849cd6
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-15 18:12:32 +0900

    마크다운 기능 등록

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flaskext.markdown import Markdown
 
 import config
 
@@ -40,5 +41,8 @@ def create_app():
     # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
+
+    # markdown
+    Markdown(app, extensions=['nl2br', 'fenced_code'])
 
     return app
