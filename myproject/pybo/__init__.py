d9f58e65245e31c009eedb9c1205bfd9dcc5306c
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-15 22:16:05 +0900

    practice

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
 
+import config
 
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -18,7 +19,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_envvar('APP_CONFIG_FILE')
+    app.config.from_object(config)
 
     # ORM
     db.init_app(app)
