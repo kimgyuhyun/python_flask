29a1db986f1e151ba7a39ca30239cc26b5eca331
kimgyuhyun <43530766+kimgyuhyun@users.noreply.github.com>
2022-02-15 18:54:08 +0900

    so에 입력된 값을 이용하여 질문 목록을 정렬할 수 있도록 _list 함수 수정

--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
 from flask import Blueprint, render_template, request, url_for, g, flash
+from sqlalchemy import func
 from werkzeug.utils import redirect
 
+
 from .. import db
-from pybo.models import Question, Answer, User
 from ..forms import QuestionForm, AnswerForm
-from pybo.views.auth_views import login_required
+from ..models import Question, Answer, User, question_voter
+from ..views.auth_views import login_required
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -16,9 +18,25 @@ def _list():
     # 입력 파라미터
     page = request.args.get('page', type=int, default=1)
     kw = request.args.get('kw', type=str, default='')
+    so = request.args.get('so', type=str, default='recent')
+
+    # 정렬
+    if so == 'recommend':
+        sub_query = db.session.query(question_voter.c.question_id, func.count('*').label('num_voter')) \
+            .group_by(question_voter.c.question_id).subquery()
+        question_list = Question.query \
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_voter.desc(), Question.create_date.desc())
+    elif so == 'popular':
+        sub_query = db.session.query(Answer.question_id, func.count('*').label('num_answer')) \
+            .group_by(Answer.question_id).subquery()
+        question_list = Question.query \
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_answer.desc(), Question.create_date.desc())
+    else:  # recent
+        question_list = Question.query.order_by(Question.create_date.desc())
 
     # 조회
-    question_list = Question.query.order_by(Question.create_date.desc())
     if kw:
         search = '%%{}%%'.format(kw)
         sub_query = db.session.query(Answer.question_id, Answer.content, User.username) \
@@ -36,7 +54,7 @@ def _list():
 
     # 페이징
     question_list = question_list.paginate(page, per_page=10)
-    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw)
+    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw, so=so)
 
 
 @bp.route('/detail/<int:question_id>/')
